596200d9c11b639fcc75517bb705886a669cee5f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-05 17:54:07 -0400

    only load a_star for now

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
+dofile("libraries/a_star.lua")
+
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest", 16)
 end
 
 function love.update()
-
+    love.event.quit()
 end
 
 function love.draw()
