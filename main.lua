fdceae53316c41c6e3cc7a67c3d2e27ece1ab67d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-05 22:10:59 -0400

    flesh out testing

--- main.lua
+++ main.lua
@@ -6,13 +6,23 @@ function love.keypressed(key)
     end
 end
 
+-- 0 nothing
+-- 1 wall
+-- 2 path
 local test_map = {
-    {0,1,1},
-    {0,1,0},
-    {1,0,0}
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,0,1,0,0,0,0,0,1,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1},
 }
 
-local debug_map = map:new(16, 4, 4, test_map)
+local debug_map = map:new(16, 10, 10, test_map)
 
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest", 16)
@@ -26,8 +36,17 @@ function love.draw()
     for x = 0,debug_map.size_x do
         for y = 0,debug_map.size_y do
             local value = debug_map:get_2d(x,y)
+
             if value > 0 then
-                love.graphics.rectangle("fill",x * 20,y * 20, 16,16)
+
+                if value == 1 then
+                    love.graphics.setColor(0.5,0.5,0.5,1)
+                elseif value == 2 then
+                    love.graphics.setColor(1,0,0,1)
+                elseif value == 3 then
+                    love.graphics.setColor(0,0,1,1)
+                end
+                love.graphics.rectangle("fill", x * 20, y * 20, 16,16)
             end
         end
     end
